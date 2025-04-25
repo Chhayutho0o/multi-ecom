7d8e95637ab2fad00d2c000ccca854a1ac128839
Cheii <chhayuth0@gmail.com>
2025-04-25 17:00:38 +0700

    customize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   const variants = [
@@ -8,14 +12,21 @@ export default function Home() {
     "secondary",
     "ghost",
     "link",
+    "elevated",
   ] as const;
   return (
-    <div className="flex gap-2 p-20">
-      {variants.map((variant) => (
-        <Button key={variant} variant={variant}>
-          {variant}
-        </Button>
-      ))}
+    <div className="flex p-20 flex-col gap-6">
+      <div className="flex gap-2">
+        {variants.map((variant) => (
+          <Button className="capitalize" key={variant} variant={variant}>
+            {variant}
+          </Button>
+        ))}
+      </div>
+      <Input placeholder="Type your message here." />
+      <Progress value={33} />
+      <Textarea placeholder="Type your message here." />
+      <Checkbox />
     </div>
   );
 }
